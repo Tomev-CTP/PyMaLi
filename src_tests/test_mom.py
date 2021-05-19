69a1fa6f46352a5e13972d760104d8d29d6c3f6b
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2021-05-19 10:53:58 +0200

    feat(mom): Add tests for mom and pimom.

--- src_tests/test_mom.py
+++ src_tests/test_mom.py
@@ -6,11 +6,28 @@ __author__ = "Tomasz Rybotycki"
 
 from unittest import TestCase
 from ..src.MOM.median_of_means import mom_mean, pimom_mean
+from numpy import mean
+from numpy.random import choice, standard_t
 
 class MOMTests(TestCase):
 
-    def test_mom_accuracy_on_students_distribution(self):
-        self.assertTrue(True)
+    def __init__(self, *args, **kwargs):
+        super(MOMTests, self).__init__(*args, **kwargs)
+        self._population = standard_t(df=1, size=1000)
+        self._sample = choice(self._population, size=100)
 
     def test_mom_accuracy_on_students_distribution(self):
-        self.assertTrue(False)
+        population_mean = mean(self._population)
+        sample_mean = mean(self._sample)
+        sample_mom_mean = mom_mean(self._sample, 10)
+        sample_mean_error_sq = ((population_mean - sample_mean)**2)
+        sample_mom_mean_error_sq = ((population_mean - sample_mom_mean)**2)
+        self.assertGreaterEqual(sample_mean_error_sq, sample_mom_mean_error_sq)
+
+    def test_mom_accuracy_on_students_distribution(self):
+        population_mean = mean(self._population)
+        sample_mean = mean(self._sample)
+        sample_pimom_mean = pimom_mean(self._sample, 10)
+        sample_mean_error_sq = ((population_mean - sample_mean) ** 2)
+        sample_pimom_mean_error_sq = ((population_mean - sample_pimom_mean) ** 2)
+        self.assertGreaterEqual(sample_mean_error_sq, sample_pimom_mean_error_sq)
