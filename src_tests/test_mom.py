bef35651b5c6debdb3eac4e573818372217c667e
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2021-05-19 10:34:34 +0200

    feat(mom): Added mom tests file.

---
+++ src_tests/test_mom.py
@@ -0,0 +1,16 @@
+__author__ = "Tomasz Rybotycki"
+
+"""
+    This script contains median of means estimator tests.
+"""
+
+from unittest import TestCase
+from ..src.MOM.median_of_means import mom_mean, pimom_mean
+
+class MOMTests(TestCase):
+
+    def test_mom_accuracy_on_students_distribution(self):
+        self.assertTrue(True)
+
+    def test_mom_accuracy_on_students_distribution(self):
+        self.assertTrue(False)
